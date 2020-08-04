d2183c4f55cc3a17a709802e9ed4e24ef8185f1a
George Saveliev <George_Saveliev@hotmail.com>
2020-08-04 09:08:45 +0300

    enforce TLS 1.2

---
+++ DotNetNuke.Customizations.Security/ServiceRouteMapper.cs
@@ -0,0 +1,18 @@
+﻿#region Usings
+
+using System.Net;
+using DotNetNuke.Web.Api;
+
+#endregion
+
+namespace DotNetNuke.Customizations.Security
+{
+    public class ServiceRouteMapper : IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            // Enable TLS 1.2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; // .NET 4.5
+        }
+    }
+}
